5eda00068cbc9f4ca5eb40f2a937b2642994614c
Josh Borrow <josh@joshborrow.com>
2025-02-28 13:13:43 -0500

    Raise appropraite errors in ligthcurve

--- lightcurvedb/client/lightcurve.py
+++ lightcurvedb/client/lightcurve.py
@@ -9,6 +9,8 @@ from psycopg import AsyncConnection
 from pydantic import BaseModel
 from sqlalchemy import select
 
+from lightcurvedb.client.band import BandNotFound
+from lightcurvedb.client.source import SourceNotFound
 from lightcurvedb.models.band import Band, BandTable
 from lightcurvedb.models.flux import FluxMeasurementTable
 from lightcurvedb.models.source import Source, SourceTable
@@ -47,6 +49,11 @@ class LightcurveResult(BaseModel):
 async def lightcurve_read_band(
     id: int, band_name: str, conn: AsyncConnection
 ) -> LightcurveBandResult:
+    band = await conn.get(BandTable, band_name)
+
+    if band is None:
+        raise BandNotFound
+
     query = select(FluxMeasurementTable)
 
     query = query.filter(FluxMeasurementTable.source_id == id)
@@ -62,12 +69,15 @@ async def lightcurve_read_band(
         for x in BAND_RESULT_ITEMS:
             outputs[x].append(getattr(item, x))
 
-    band = await conn.get(BandTable, band_name)
-
     return LightcurveBandResult(band=band.to_model(), **outputs)
 
 
 async def lightcurve_read_source(id: int, conn: AsyncConnection) -> LightcurveResult:
+    source = await conn.get(SourceTable, id)
+
+    if source is None:
+        raise SourceNotFound
+
     query = select(FluxMeasurementTable.band_name)
     query = query.filter(FluxMeasurementTable.source_id == id)
     query = query.distinct()
@@ -76,7 +86,5 @@ async def lightcurve_read_source(id: int, conn: AsyncConnection) -> LightcurveRe
 
     bands = [lightcurve_read_band(id=id, band_name=b, conn=conn) for b in band_names]
     bands = await asyncio.gather(*bands)
-
-    source = await conn.get(SourceTable, id)
 
     return LightcurveResult(source=source.to_model(), bands=bands)
