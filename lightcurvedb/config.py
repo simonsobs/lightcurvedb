67866b327f4fdb368000c7cc8d1658d7b8e0aee6
Divesh <divesh.jain14@gmail.com>
2025-11-21 13:33:59 +0000

    added comfiguration for psycopg connection

--- lightcurvedb/config.py
+++ lightcurvedb/config.py
@@ -2,11 +2,15 @@
 Configuration options for the lightcurvedb when running in a fixed
 environment.
 """
+from enum import Enum
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .managers import AsyncSessionManager, SyncSessionManager
 
+class ConnectionType(str, Enum):
+    SQLALCHEMY = "sqlalchemy"
+    PSYCOPG = "psycopg"
 
 class Settings(BaseSettings):
     postgres_user: str = "postgres"
@@ -16,12 +20,17 @@ class Settings(BaseSettings):
     postgres_db: str = "lightcurvedb"
 
     postgres_echo: bool = False
+    connection_type: ConnectionType = ConnectionType.PSYCOPG
 
     model_config = SettingsConfigDict(env_prefix="LIGHTCURVEDB_")
 
     @property
     def postgres_uri(self) -> str:
         return f"postgresql+psycopg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+
+    @property
+    def psycopg_uri(self) -> str:
+        return f"postgresql://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
 
     def sync_manager(self) -> SyncSessionManager:
         return SyncSessionManager(
@@ -32,9 +41,9 @@ class Settings(BaseSettings):
     def async_postgres_uri(self) -> str:
         return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
 
-    def async_manager(self) -> SyncSessionManager:
+    def async_manager(self) -> AsyncSessionManager:
         return AsyncSessionManager(
-            connection_url=self.postgres_uri, echo=self.postgres_echo
+            connection_url=self.async_postgres_uri, echo=self.postgres_echo
         )
 
 
