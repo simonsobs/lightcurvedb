0e4340369319280d19235029e540332cb7662769
Josh Borrow <josh@joshborrow.com>
2026-02-05 14:00:09 -0500

    Remove unused cutout table

--- lightcurvedb/models/cutout.py
+++ lightcurvedb/models/cutout.py
@@ -27,32 +27,3 @@ class Cutout(BaseModel):
     band_name: str | None = None
     flux_id: int | None = None
 
-
-class CutoutTable(SQLModel, Cutout, table=True):
-    id: int = Field(primary_key=True)
-    data: list[list[float]] = Field(sa_type=ARRAY(FLOAT))
-
-    band_name: str | None = Field(default=None, foreign_key="bands.name")
-    band: "BandTable" = Relationship()
-
-    source_id: int | None = Field(default=None, foreign_key="sources.id")
-    source: "SourceTable" = Relationship()
-
-    flux_id: int = Field()
-    flux: "FluxMeasurementTable" = Relationship()
-    __table_args__ = (
-        ForeignKeyConstraint(
-            ["flux_id", "time"],
-            ["flux_measurements.id", "flux_measurements.time"],
-        ),
-    )
-
-    def to_model(self) -> Cutout:
-        return Cutout(
-            id=self.id,
-            data=self.data,
-            band_name=self.band_name,
-            flux_id=self.flux_id,
-            time=self.time,
-            units=self.units,
-        )
