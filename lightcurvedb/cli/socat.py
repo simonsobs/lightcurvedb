a986efe7574e01a621bbb20083ecc3ac09c4134e
Josh Borrow <josh@joshborrow.com>
2026-02-17 09:19:02 -0500

    Fix socat script

--- lightcurvedb/cli/socat.py
+++ lightcurvedb/cli/socat.py
@@ -2,20 +2,26 @@
 Update the sources registered in lightcurvedb to match those from socat.
 """
 
+import asyncio
+
+from lightcurvedb.integrations.socat import upsert_sources
+
+
+async def core(lightcurvedb_settings, socat_settings):
+    async with lightcurvedb_settings.backend as backend:
+        await upsert_sources(
+            client=socat_settings.client,
+            backend=backend,
+            progress_bar=True,
+        )
+
 
 def main():
     from socat.client import settings as socat_settings
 
     from lightcurvedb.config import settings as lightcurvedb_settings
-    from lightcurvedb.integrations.socat import upsert_sources
 
-    manager = lightcurvedb_settings.sync_manager()
-    manager.create_all()
-
-    settings = socat_settings.SOCatClientSettings()
-
-    with manager.session() as session:
-        upsert_sources(client=settings.client, session=session, progress_bar=True)
+    asyncio.run(core(lightcurvedb_settings, socat_settings))
 
 
 if __name__ == "__main__":
